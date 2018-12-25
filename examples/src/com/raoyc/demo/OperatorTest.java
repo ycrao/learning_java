b1012f5ec9b860bf99708e58ee08bbb1506aa787
raoyc <raoyc@foxmail.com>
2018-12-25 19:01:40 +0800

    完善例子

--- examples/src/com/raoyc/demo/OperatorTest.java
+++ examples/src/com/raoyc/demo/OperatorTest.java
@@ -27,5 +27,74 @@ public class OperatorTest {
         System.out.println("e < f = " + (e < f));
         System.out.println("e >= f = " + (e >= f));
         System.out.println("e <= f = " + (e <= f));
+
+        a = 60; /* 60 = 0011 1100 */
+        b = 13; /* 13 = 0000 1101 */
+        c = 0;
+        c = a & b;       /* 12 = 0000 1100 */
+        System.out.println("a & b = " + c);
+
+        c = a | b;       /* 61 = 0011 1101 */
+        System.out.println("a | b = " + c);
+
+        c = a ^ b;       /* 49 = 0011 0001 */
+        System.out.println("a ^ b = " + c);
+
+        c = ~a;          /*-61 = 1100 0011 */
+        System.out.println("~a = " + c);
+
+        c = a << 2;     /* 240 = 1111 0000 */
+        System.out.println("a << 2 = " + c);
+
+        c = a >> 2;     /* 15 = 1111 */
+        System.out.println("a >> 2  = " + c);
+
+        c = a >>> 2;     /* 15 = 0000 1111 */
+        System.out.println("a >>> 2 = " + c);
+
+        boolean x = true;
+        boolean y = false;
+        System.out.println("x && y = " + (x && y));
+        System.out.println("x || y = " + (x || y));
+        System.out.println("!(x && y) = " + !(x && y));
+
+        a = 10;
+        b = 20;
+        c = 0;
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a ;
+        System.out.println("c += a  = " + c);
+        c -= a ;
+        System.out.println("c -= a = " + c);
+        c *= a ;
+        System.out.println("c *= a = " + c);
+        a = 10;
+        c = 15;
+        c /= a ;
+        System.out.println("c /= a = " + c);
+        a = 10;
+        c = 15;
+        c %= a ;
+        System.out.println("c %= a  = " + c);
+        c <<= 2 ;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2 ;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2 ;
+        System.out.println("c >>= 2 = " + c);
+        c &= a ;
+        System.out.println("c &= a  = " + c);
+        c ^= a ;
+        System.out.println("c ^= a   = " + c);
+        c |= a ;
+        System.out.println("c |= a   = " + c);
+
+        b = (a == 1) ? 20 : 30;
+        System.out.println( "Value of b is : " + b);
+
+        String name = "James";
+        boolean result = name instanceof String; // 由于 name 是 String 类型，所以返回真
+        System.out.println("result is " + result);
     }
 }

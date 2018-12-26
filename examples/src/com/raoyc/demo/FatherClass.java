8450c3071f371b0f6b11ed9fe859e68379812094
raoyc <raoyc@foxmail.com>
2018-12-26 17:28:48 +0800

    father and son class

--- examples/src/com/raoyc/demo/FatherClass.java
+++ examples/src/com/raoyc/demo/FatherClass.java
@@ -1,4 +1,21 @@
 package com.raoyc.demo;
 
+// see https://github.com/brianway/java-learning/blob/master/java-base/src/main/java/com/brianway/learning/java/base/constructor/FatherClass.java
 public class FatherClass {
+    protected static int count = 10;
+    private String name;
+
+    static {  // 静态代码块
+        System.out.println("父类的静态属性count初始化:" + count);
+    }
+
+    public FatherClass() {
+        System.out.println("执行了父类的无参构造方法");
+    }
+
+    public FatherClass(String name) {
+        this.name = name;
+        System.out.println("执行了父类的构造方法FatherClass(String name) " + name);
+    }
 }
+
--- examples/src/com/raoyc/demo/SonClass.java
+++ examples/src/com/raoyc/demo/SonClass.java
@@ -1,4 +1,25 @@
 package com.raoyc.demo;
 
-public class SonClass {
+// see https://github.com/brianway/java-learning/blob/master/java-base/src/main/java/com/brianway/learning/java/base/constructor/SonClass.java
+public class SonClass extends FatherClass {
+    private static int countSon;
+
+    static {
+        System.out.println("子类可以访问父类的静态属性count " + count);
+        System.out.println("子类的静态属性countSon初始化:" + countSon);
+    }
+
+    public SonClass(String name) {
+        //super(name);
+        System.out.println("执行了子类的构造方法SonClass(String name) " + name);
+    }
+
+    public SonClass() {
+        System.out.println("执行了子类的无参构造方法");
+    }
+
+    public static void main(String[] args) {
+        new SonClass("aaa");
+        new SonClass();
+    }
 }
